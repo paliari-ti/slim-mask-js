21dc7a0e4001e3b8ea23573409e02342a7516f72
Felipe Bohnert Paetzold <felipe.paetzold@gmail.com>
2018-11-27 17:58:36 -0200

    fix dev dependencies

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    library: 'slim-mask-js',
+    libraryTarget: 'umd',
+    filename: 'slim-mask-js.js',
+    auxiliaryComment: 'A slim mask for JS'
+  }
+}
