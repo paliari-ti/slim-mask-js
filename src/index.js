e9d29e5949d6995df4be9201e8edd7407cef757f
Felipe Bohnert Paetzold <felipe.paetzold@gmail.com>
2018-11-27 17:59:22 -0200

    add src/

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import maskit from './lib/maskit'
+import masker from './masker'
+
+export { maskit }
+
+export default masker
---
+++ src/masker.js
@@ -0,0 +1,4 @@
+import maskit from './lib/maskit'
+import tokens from './lib/tokens'
+
+export default (value, mask) => maskit(value, mask, tokens)
